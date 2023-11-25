9e3c73eeed4f84c669adebe721bcc3b7f6375e77
shadow6777 <spires45@gmail.com>
2023-11-25 03:21:09 -0500

    static HTML templates with JSON

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const hbs = require("hbs");
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
