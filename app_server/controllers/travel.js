efb4fd94723b3f1ff69c133f19c97092ceae74ab
shadow6777 <spires45@gmail.com>
2023-12-02 11:42:34 -0500

    HTML templates with JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,12 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
 
 /* GET travel view */
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' -travel';
-    res.render('travel', { title: 'Travlr Getaways', trips});
+  pageTitle = process.env.npm_package_description + " -travel";
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 
 module.exports = {
-    travel
+  travel,
 };
