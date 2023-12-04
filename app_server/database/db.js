f396bad0f1b64cd45ae20bf3efdfd69215534b8b
shadow6777 <spires45@gmail.com>
2023-12-04 02:51:50 -0500

    NoSQL Databases,Models, and Schemas

---
+++ app_server/database/db.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURI = 'mongodb://${host}/travlr';
+const readLine = require('readline');
+
+//avoid 'current server discovery and monitoring engine is deprecated;
+mongoose.set('useUnifiedTopology', true);
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+});
+
+mongoose.connection.on('error', err => {
+});
+
+mongoose.connection.on('disconnected', () => {
+});
+
+if (process.platform === 'win32') {
+}
+
+const gracefulShutdown = (msg, callback) => {
+};
+
+//for nodemon restarts
+process.once('SIGUSR2', () => {
+});
+
+//for app termination
+process.on('SIGINT', () => {
+});
+
+//for Heroku app termination
+process.on('SIGTERM', () => {
+});
+
+connect();
+
+//bring in the mongoose schema
+require('./travlr');
