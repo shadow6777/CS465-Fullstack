322ea147263d16719a4b944caa6acf60add796c3
shadow6777 <spires45@gmail.com>
2023-12-10 02:50:56 -0500

    RESTful api

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose'); //.set('debug', true);
+const Model = mongoose.model('trips');
+
+//GET: /trips - list all the trips
+const tripsList = async (req, red) => {
+    Model
+        .find({}) //empty filter for all
+        .exec((err, trips) => {
+            if (!trips) {
+                return res
+                    .status(404)
+                    .json({"message" : "trips not found"});
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else {
+                return res
+                    .status(200)
+                    .json(trips);
+            }
+        });
+};
+
+//GET: /trips/:tripCode -return a single trip
+const tripsFindCode = async (req, res) => {
+    Model
+        .find({ 'Code': req.params.tripCode})
+        .exec((err, trip) => {
+            if(!trip) {
+                return res
+                    .status(404)
+                    .json({"message": "trip not found"});
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else {
+                return res
+                    .status(200)
+                    .json(trip);
+            }
+        });
+};
+
+module.exports = {
+    tripsList,
+    tripsFindCode
+};
